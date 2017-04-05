156daa94c5a57af3b11bdbb939ebab43268e47d4
orancanoren <orancanoren@gmail.com>
2017-04-05 17:41:00 +0300

    adjusted for heroku

---
+++ public/main.js
@@ -0,0 +1,5 @@
+var push_button = document.getElementById('push_button');
+
+push_button.addEventListener('click', function() {
+  location.href="https://murmuring-forest-45186.herokuapp.com/push";
+})
